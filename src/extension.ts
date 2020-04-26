9b7e9f45d0900705404dc76989c7e837173d56fd
Ben Gordon <bunookana@gmail.com>
2020-04-26 13:36:06 -0700

    Tree view showing devices

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,10 @@
 import * as vscode from 'vscode';
-import { chuckJoke } from './chuckJoke';
-import { request } from 'https';
 
+import { request } from 'https';
+import { chuckJoke } from './chuckJoke';
+import { carTreeDataProvider } from './carTreeView';
+import { DepNodeProvider, Dependency } from './nodeDependencies';
+import { F5TreeProvider } from './treeProvider';
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -25,19 +28,61 @@ export function activate(context: vscode.ExtensionContext) {
 	// 	2.	configure extension config to manage device list 
 	//		2a.	settings structure to host devices - DONE
 	//		2b.	command to quickly bring up settings - DONE
-	//		2c.	quickPick list of hosts - just log what was selected
-	//	3.	inputBox for passwords, keyring to store them
-	//	4.	tree view in F5 container
-	//		4a.	F5 tree view contianer with f5 logo
-	//		4b.	display hosts from config file in tree view
+	//		2c.	quickPick list of hosts - just log what was selected - DONE
+	//	3.	inputBox for passwords - DONE
+	//	4.	keyring to store passwords
+	//			- node-keytar - https://github.com/atom/node-keytar
+	//	5.	tree view in F5 container
+	//		4a.	F5 tree view contianer with f5 logo - DONE
+	//		4b.	display hosts from config file in tree view - DONE
 	//		4c. add/edit/delete device from tree view
-	//	5.	configure tests?  mocha?
-	//	6.	list fast templates under device in tree view
+	//	6.	configure tests?  mocha?
+	//	7.	list fast templates under device in tree view
 	//		6a.	when selected in tree view, display template in editor
-	//	7.	list deployed applications in tree view
+	//	8.	list deployed applications in tree view
 	//		7a.	when selected in tree view, display template in editor
-	//	8.	configure JSON output highlighting like RestClient
+	//	9.	configure JSON output highlighting like RestClient
 
+
+	context.subscriptions.push(vscode.commands.registerCommand('f5-fast.connectDevice', async () => {
+		console.log('executing f5-fast.connectDevice');
+
+		// get config and map bigip hosts to items list for quickPick
+		const bigipHosts = vscode.workspace.getConfiguration().get('f5-fast.hosts', ['']).map(label => ({ label }));
+		// fire quickPick, assign selection
+		const bigipHost1 = await vscode.window.showQuickPick(bigipHosts, {placeHolder: 'select device'}).then(
+			host => console.log(`Selected host: ${JSON.stringify(host)}`)
+			);
+			console.log(`Selected bigipHost1: ${bigipHost1}`);
+			
+
+		// another way to do this
+		const bigipHostsNew = vscode.workspace.getConfiguration().get('f5-fast.hosts');
+		vscode.window.showQuickPick(Promise.resolve(bigipHostsNew)).then(picked => console.log(picked));
+		console.log(`bigipHostsNew: ${bigipHostsNew}`)
+
+			// let bigipHost = JSON.stringify(bigipHost1.label);
+		// bigipHost = bigipHost.label;
+		// vscode.window.showInformationMessage(`Selected device: ${bigipHost}`);
+
+		
+		debugger;
+	}));
+	
+	context.subscriptions.push(vscode.commands.registerCommand('f5-fast.connectDevice2', async () => {
+		console.log('executing f5-fast.connectDevice2');
+		
+		const bigipHosts = vscode.workspace.getConfiguration().get('f5-fast.hosts');
+
+		const bigip = await vscode.window.showQuickPick(bigipHosts);
+		console.log(`Selected device/host/bigip: ${bigip}`);
+		vscode.window.showInformationMessage(`Selected device/host/bigip = ${bigip}`)
+
+		const password = await vscode.window.showInputBox({password: true});
+		console.log(`Password: ${password}`);
+
+
+	}));
 
 	context.subscriptions.push(vscode.commands.registerCommand('chuckJoke', () => {
 		// console.log('befor Chuck func call');
@@ -55,11 +100,33 @@ export function activate(context: vscode.ExtensionContext) {
 	
 //original way the example extension structured the command
 	let disposable = vscode.commands.registerCommand('extension.remoteCommand', async () => {
-		vscode.window.showInformationMessage('placeholder to execute command on bigip...')
+		return vscode.window.showInformationMessage('placeholder to execute command on bigip...')
 	});	
 	context.subscriptions.push(disposable);
+
+	// trying to setup tree provider to list f5 hosts from config file
+	// const hostsTreeProvider = new F5TreeProvider(vscode.workspace.getConfiguration().get('f5-fast.hosts'));
+	const hostsTreeProvider = new F5TreeProvider(vscode.workspace.rootPath);
+	vscode.window.registerTreeDataProvider('f5Hosts', hostsTreeProvider);
+	vscode.commands.registerCommand('f5-fast.refreshEntry', () => hostsTreeProvider.refresh());
+	vscode.commands.registerCommand('f5-fast.addEntry', () => vscode.window.showInformationMessage(`Successfully called add entry.`));
+	vscode.commands.registerCommand('f5-fast.editEntry', (node: Dependency) => vscode.window.showInformationMessage(`Successfully called edit entry on ${node.label}.`));
+	vscode.commands.registerCommand('f5-fast.deleteEntry', (node: Dependency) => vscode.window.showInformationMessage(`Successfully called delete entry on ${node.label}.`));
+
+	// Samples of `window.registerTreeDataProvider`
+	// all entries for nodeDependeny tree view
+	const nodeDependenciesProvider = new DepNodeProvider(vscode.workspace.rootPath);
+	vscode.window.registerTreeDataProvider('nodeDependencies', nodeDependenciesProvider);
+	vscode.commands.registerCommand('nodeDependencies.refreshEntry', () => nodeDependenciesProvider.refresh());
+	vscode.commands.registerCommand('extension.openPackageOnNpm', moduleName => vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(`https://www.npmjs.com/package/${moduleName}`)));
+	vscode.commands.registerCommand('nodeDependencies.addEntry', () => vscode.window.showInformationMessage(`Successfully called add entry.`));
+	vscode.commands.registerCommand('nodeDependencies.editEntry', (node: Dependency) => vscode.window.showInformationMessage(`Successfully called edit entry on ${node.label}.`));
+	vscode.commands.registerCommand('nodeDependencies.deleteEntry', (node: Dependency) => vscode.window.showInformationMessage(`Successfully called delete entry on ${node.label}.`));
+
+
+	vscode.window.registerTreeDataProvider('carTree', new carTreeDataProvider());
+
+
 }
-
-
 // this method is called when your extension is deactivated
 export function deactivate() {}
---
+++ src/treeProvider.ts
@@ -0,0 +1,184 @@
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+
+export class F5TreeProvider implements vscode.TreeDataProvider<f5Host> {
+
+	private _onDidChangeTreeData: vscode.EventEmitter<f5Host | undefined> = new vscode.EventEmitter<f5Host | undefined>();
+	readonly onDidChangeTreeData: vscode.Event<f5Host | undefined> = this._onDidChangeTreeData.event;
+
+	constructor(private workspaceRoot: string) {
+	}
+
+	refresh(): void {
+		this._onDidChangeTreeData.fire();
+	}
+
+	getTreeItem(element: f5Host): vscode.TreeItem {
+		return element;
+	}
+
+	getChildren(element?: f5Host): Thenable<f5Host[]> {
+		// if (!this.workspaceRoot) {
+        //     vscode.window.showInformationMessage('No dependency in empty workspace');
+        //     console.log(`!this.workspaceRoot: ${this.workspaceRoot}`)
+        //     console.log(`!this.workspaceRoot-element: ${element}`)
+		// 	return Promise.resolve([]);
+		// }
+        
+        const bigipHosts = vscode.workspace.getConfiguration().get('f5-fast.hosts');
+        console.log(`bigips: ${JSON.stringify(bigipHosts)}`);
+        
+        // const data = fs.readFileSync(path.resolve(__dirname, '../src/hosts.json'), 'utf8');
+        // console.log(`fileData: ${data}`);
+        // const data2 = JSON.parse(data);
+        // console.log(`fileDataAsJSON: ${data2}`);
+        // const data3 = JSON.stringify(data2);
+        // console.log(`fileDataAsJSON: ${data3}`);
+        
+
+        
+        const toDep = (name: string): f5Host => {
+            // const createHost = 
+            const newDep = new f5Host(name, vscode.TreeItemCollapsibleState.None, {
+                command: 'f5-fast.connectDevice',
+                title: '',
+                arguments: [element]
+            });
+            console.log(`newDep host1: ${JSON.stringify(newDep)}`);
+            return newDep;
+        }
+
+        const depsAgain = bigipHosts.map(host => toDep(host));
+
+        console.log(`depsAgain full: ${JSON.stringify(depsAgain)}`);
+
+        // return Promise.resolve(data2);
+        return Promise.resolve(depsAgain);
+
+        // const hostDep = new Dependency(moduleName, version, vscode.TreeItemCollapsibleState.Collapsed);
+
+		// if (element) {
+        //     console.log(`element in getChildren: ${element}`)
+		// 	return Promise.resolve(this.getDepsInPackageJson(path.join(this.workspaceRoot, 'node_modules', element.label, 'package.json')));
+		// } else {
+        //     const packageJsonPath = path.join(this.workspaceRoot, 'package.json');
+        //     console.log(`packageJsonPath: ${packageJsonPath}`)
+            
+		// 	if (this.pathExists(packageJsonPath)) {
+        //         const packageStuff = this.getDepsInPackageJson(packageJsonPath);
+        //         console.log(`\r\npackageStuff: ${JSON.stringify(packageStuff)}`)
+		// 		// return Promise.resolve(packageStuff);
+		// 		return Promise.resolve(data2);
+		// 	} else {
+		// 		vscode.window.showInformationMessage('Workspace has no package.json');
+		// 		return Promise.resolve([]);
+		// 	}
+		// }
+
+	}
+    
+
+	/**
+	 * Given the path to package.json, read all its dependencies and devDependencies.
+	 */
+	// private getDepsInPackageJson(packageJsonPath: string): Dependency[] {
+	// 	if (this.pathExists(packageJsonPath)) {
+    //         const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
+
+
+
+	// 		const toDep = (moduleName: string, version: string): Dependency => {
+	// 			if (this.pathExists(path.join(this.workspaceRoot, 'node_modules', moduleName))) {
+    //                 const firstDep = new Dependency(moduleName, version, vscode.TreeItemCollapsibleState.Collapsed);
+    //                 console.log(`firstDep: ${JSON.stringify(firstDep)}`)
+    //                 return firstDep;
+	// 			} else {
+    //                 const secondDep = new Dependency(moduleName, version, vscode.TreeItemCollapsibleState.None, {
+	// 					command: 'extension.openPackageOnNpm',
+	// 					title: '',
+	// 					arguments: [moduleName]
+    //                 });
+    //                 console.log(`secondDep: ${JSON.stringify(secondDep)}`)
+    //                 return secondDep; 
+	// 			}
+	// 		};
+            
+	// 		const deps = packageJson.dependencies
+    //         ? Object.keys(packageJson.dependencies).map(dep => toDep(dep, packageJson.dependencies[dep]))
+    //         : [];
+	// 		const devDeps = packageJson.devDependencies
+    //         ? Object.keys(packageJson.devDependencies).map(dep => toDep(dep, packageJson.devDependencies[dep]))
+    //         : [];
+    //         const fullDeps = deps.concat(devDeps);
+    //         console.log(`\r\nfullDep List: ${JSON.stringify(fullDeps)}`)
+    //         return fullDeps;
+	// 	} else {
+	// 		return [];
+	// 	}
+	// }
+
+	// private pathExists(p: string): boolean {
+	// 	try {
+	// 		fs.accessSync(p);
+	// 	} catch (err) {
+	// 		return false;
+	// 	}
+	// 	return true;
+	// }
+}
+
+export class f5Host extends vscode.TreeItem {
+	constructor(
+		public readonly label: string,
+		// private version: string,
+		public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+		public readonly command?: vscode.Command
+	) {
+		super(label, collapsibleState);
+	}
+
+	// get tooltip(): string {
+	// 	return `tooltip-version`;
+	// }
+
+	// get description(): string {
+	// 	return 'descLoc';
+	// }
+
+	// iconPath = {
+	// 	light: path.join(__filename, '..', '..', 'resources', 'light', 'dependency.svg'),
+	// 	dark: path.join(__filename, '..', '..', 'resources', 'dark', 'dependency.svg')
+	// };
+
+    // contextValue = 'dependency';
+    
+}
+
+// export class Dependency extends vscode.TreeItem {
+
+// 	constructor(
+// 		public readonly label: string,
+// 		private version: string,
+// 		public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+// 		public readonly command?: vscode.Command
+// 	) {
+// 		super(label, collapsibleState);
+// 	}
+
+// 	// get tooltip(): string {
+// 	// 	return `tooltip-version`;
+// 	// }
+
+// 	// get description(): string {
+// 	// 	return 'descLoc';
+// 	// }
+
+// 	// iconPath = {
+// 	// 	light: path.join(__filename, '..', '..', 'resources', 'light', 'dependency.svg'),
+// 	// 	dark: path.join(__filename, '..', '..', 'resources', 'dark', 'dependency.svg')
+// 	// };
+
+// 	// contextValue = 'dependency';
+
+// }
