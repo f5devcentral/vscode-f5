0231e8ba4ed7da05aae97a1ac4027e8365bedae7
Ben Gordon <Bunookana@gmail.com>
2020-06-22 08:46:45 -0400

    updated tree with do examples (#33)
    Co-authored-by: Ben Gordon <b.gordon@f5.com>

--- src/treeViewsProviders/githubDecExamples.ts
+++ src/treeViewsProviders/githubDecExamples.ts
@@ -10,7 +10,7 @@ export class ExampleDecsProvider implements vscode.TreeDataProvider<ExampleDec> 
 	private _onDidChangeTreeData: vscode.EventEmitter<ExampleDec | undefined> = new vscode.EventEmitter<ExampleDec | undefined>();
 	readonly onDidChangeTreeData: vscode.Event<ExampleDec | undefined> = this._onDidChangeTreeData.event;
 
-	constructor(private inCommingData: string) {
+	constructor() {
 	}
 
 	refresh(): void {
@@ -22,57 +22,84 @@ export class ExampleDecsProvider implements vscode.TreeDataProvider<ExampleDec> 
 	}
 
 	async getChildren(element?: ExampleDec): Promise<ExampleDec[]> {
-        
-		const decCall = await callHTTPS({
-		    method: 'GET',
-		    host: 'api.github.com',
-		    path: '/repos/F5Networks/f5-telemetry-streaming/contents/examples/declarations',
-		    headers: {
-		        'Content-Type': 'application/json',
-		        'User-Agent': 'nodejs native HTTPS'
-		    }
-		}).then( resp => {
-			return resp;
-		});
 
-		console.log(`decCall: ${JSON.stringify(decCall)}`);
-		
-		const treeItems = decCall.body.map((item:any): ExampleDec => {
-			return (new ExampleDec(
-				item.name, 
-				vscode.TreeItemCollapsibleState.None, 
-				{
-					command: 'f5-ts.getGitHubExampleTs',
-					title: 'hostTitle',
-					arguments: [item.download_url]
-			}));
-		});
+		var treeItems = [];
+		if(element) {
+			// get example dec list for atc service
+			if(element.label === 'AS3 Examples - Coming soon!') {
+				// const examples = await getAS3examples();
+				
+				// treeItems = examples.body.map((item: any) => {
+				// 	return new ExampleDec(item.name, vscode.TreeItemCollapsibleState.None,
+				// 		{command: 'f5.getGitHubExample', title: '', arguments: [item.download_url]});
+				// });
 
-		// if ( bigipHosts === undefined) {
-		// 	throw new Error('No configured hosts - from hostTreeProvider');
-		// }
-   
-        // // takes individual host item and creates a tree item
-        // const treeHosts = (name: string): exampleTsDec => {
-        //     const treeItem = new exampleTsDec(name, vscode.TreeItemCollapsibleState.None, {
-        //         command: 'f5.connectDevice',
-        //         title: 'hostTitle',
-        //         arguments: [name]
-        //     });
-        //     console.log(`treeItem: ${JSON.stringify(treeItem)}`);
-        //     return treeItem;
-        // }
-        // const treeItems = decCall.map(host => treeHosts(host));
+			} else if (element.label === 'DO Examples') {
+				const examples = await getDOexamples();
 
-        console.log(`treeItems full: ${JSON.stringify(treeItems)}`);
+				treeItems = examples.body.map((item: any) => {
+					return new ExampleDec(item.name, vscode.TreeItemCollapsibleState.None,
+						{command: 'f5.getGitHubExample', title: '', arguments: [item.download_url]});
+				});
 
+			} else if (element.label === 'TS Examples') {
+				const examples = await getTSexamples();
+
+				treeItems = examples.body.map((item: any) => {
+					return new ExampleDec(item.name, vscode.TreeItemCollapsibleState.None,
+						{command: 'f5.getGitHubExample', title: '', arguments: [item.download_url]});
+				});
+			}
+
+		} else {
+			//top level items
+			treeItems.push(new ExampleDec('AS3 Examples - Coming soon!', vscode.TreeItemCollapsibleState.None,
+									{command: 'f5.getGitHubExample', title: '', arguments: ['tempAS3']}));
+
+			treeItems.push(new ExampleDec('DO Examples', vscode.TreeItemCollapsibleState.Collapsed));
+
+			treeItems.push(new ExampleDec('TS Examples', vscode.TreeItemCollapsibleState.Collapsed));
+		}
         return Promise.resolve(treeItems);
-
-        
-
 	}
-    
+}
 
+async function getAS3examples(){
+	return await callHTTPS({
+		method: 'GET',
+		host: 'api.github.com',
+		path: '/repos/F5Networks/f5-telemetry-streaming/contents/examples/declarations',
+		headers: {
+			'Content-Type': 'application/json',
+			'User-Agent': 'F5 VScode FAST extension'
+		}
+	});
+}
+
+
+async function getDOexamples(){
+	return await callHTTPS({
+		method: 'GET',
+		host: 'api.github.com',
+		path: '/repos/F5Networks/f5-declarative-onboarding/contents/examples',
+		headers: {
+			'Content-Type': 'application/json',
+			'User-Agent': 'F5 VScode FAST extension'
+		}
+	});
+}
+
+
+async function getTSexamples(){
+	return await callHTTPS({
+		method: 'GET',
+		host: 'api.github.com',
+		path: '/repos/F5Networks/f5-telemetry-streaming/contents/examples/declarations',
+		headers: {
+			'Content-Type': 'application/json',
+			'User-Agent': 'F5 VScode FAST extension'
+		}
+	});
 }
 
 export class ExampleDec extends vscode.TreeItem {
@@ -84,20 +111,4 @@ export class ExampleDec extends vscode.TreeItem {
 	) {
 		super(label, collapsibleState);
 	}
-
-	get tooltip(): string {
-		return `Connect`;
-	}
-
-	// get description(): string {
-	// 	return 'descLoc';
-	// }
-
-	// iconPath = {
-	// 	light: path.join(__filename, '..', '..', 'resources', 'light', 'dependency.svg'),
-	// 	dark: path.join(__filename, '..', '..', 'resources', 'dark', 'dependency.svg')
-	// };
-
-    // contextValue = 'dependency';
-    
 }
