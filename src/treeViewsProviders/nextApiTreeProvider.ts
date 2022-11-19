3692f13cd1561bae36ca2deaddb879278a61c960
DumpySquare <bunookana@gmail.com>
2022-11-19 18:11:30 +0000

    tree work POST leaf

--- src/treeViewsProviders/nextApiTreeProvider.ts
+++ src/treeViewsProviders/nextApiTreeProvider.ts
@@ -81,35 +81,90 @@ export class NextApiTreeProvider implements TreeDataProvider<NxtApiTreeItem> {
 
             if (element) {
 
-                // get full/base path
-                const elPath = element.path?.join('.')
-                const path = pathValueFromKey(this.pathsTreeObj, element.path?.join('.') || element.label);
-                const pathKey = path?.path ? [path.path, path.key].join('.') : path.key;
+                console.log('incoming element', element.path)
 
-                // now get all the keys from that path and create tree items
-                const b = this.pathsTreeObj![pathKey];
-                Object.keys(this.pathsTreeObj![pathKey]).forEach(key => {
+                // get object for element
+                const elObj = deepGet(this.pathsTreeObj!, element.path!) as Object;
 
-                    const thisPathKey = [pathKey, key]
-                    const sub1 = pathValueFromKey(this.pathsTreeObj![pathKey], key);
-                    const sub2 = deepGet(this.pathsTreeObj![pathKey], thisPathKey);
+                const elObjKeys = Object.keys(elObj);
 
-                    // no keys, means we have a leaf
-                    if(Object.keys(sub1.value).length === 0) {
-                        
-                        // return a leaf
+                // if this object has key/children, it's branch
+                if (elObjKeys.length > 0) {
+
+                    // now get all the keys from that path and create tree items
+                    Object.keys(elObj).forEach(key => {
+
+                        // copy the path array
+                        const thisPathKey: string[] = element.path!.map(x => x);
+                        thisPathKey.push(key)   // append local key to array path
+
+
+                        const sub2 = deepGet(this.pathsTreeObj!, thisPathKey) as Object;
+                        const sub2Keys = Object.keys(sub2)
+
+                        const leaf1 = thisPathKey.map(x=>x);   // add an empty element to the front for the join to append '/' to the beginning
+                        leaf1.unshift('')
+                        const pathString = leaf1.join('/')  // join to make original path
+                        const leafObj = this.oai!.paths[pathString];    // get the full details from this object/path
+
+                        // if leafObj has get/post/put, create action items
+                        if(leafObj?.hasOwnProperty('post')) {
+                            // if we have post details
+                            // add in example post and provide codeLense to post payload
+
+                            // examples location:  /api/change-password.post.requestBody.content.application/json.example
+                            // scehema location:  /api/change-password.post.requestBody.content.application/json.schema
+
+                            const schemaRef = leafObj.post!.requestBody!.content!['application/json']!.schema!['$ref']!;
+                            const example = leafObj?.post?.requestBody?.content?.['application/json']?.example;
+                            treeItems.push(
+                                new NxtApiTreeItem('POST', '', '', '', 'nextApiTreeItem', thisPathKey, TreeItemCollapsibleState.None,
+                                {title: '', command: 'oaiPost', arguments: [new OaiPost(
+                                    pathString, schemaRef, example
+                                )]})
+                            );
+                        }
+
+
+                        // no keys, means we have a leaf
+                        if (sub2Keys.length === 0) {
+
+                            // return a leaf
+                            treeItems.push(
+                                new NxtApiTreeItem(key, 'leaf', '', '', 'nextApiTreeItem', thisPathKey, TreeItemCollapsibleState.Collapsed)
+                            );
+
+                        } else {
+
+                            // return a branch
+                            treeItems.push(
+                                new NxtApiTreeItem(key, 'branch', '', '', 'nextApiTreeItem', thisPathKey, TreeItemCollapsibleState.Collapsed)
+                            );
+                        }
+                    })
+
+                } else {
+                    // since no keys/children, return leaf details
+                    element.path!.unshift('')   // add an empty element to the front for the join to append '/' to the beginning
+                    const pathString = element.path!.join('/')  // join to make original path
+                    const leafObj = this.oai!.paths[pathString];    // get the full details from this object/path
+
+                    if(leafObj.hasOwnProperty('post')) {
+                        // if we have post details
                         treeItems.push(
-                            new NxtApiTreeItem(key, 'leaf', '', '', 'nextApiTreeItem', thisPathKey, TreeItemCollapsibleState.Collapsed)
-                        );
-
-                    } else {
-                        
-                        // return a branch
-                        treeItems.push(
-                            new NxtApiTreeItem(key, 'branch', '', '', 'nextApiTreeItem', thisPathKey, TreeItemCollapsibleState.Collapsed)
+                            new NxtApiTreeItem('POST', 'leaf', '', '', 'nextApiTreeItem', [], TreeItemCollapsibleState.None)
                         );
                     }
-                })
+
+                    if(leafObj.hasOwnProperty('put')) {
+                        // if we have put details
+                        treeItems.push(
+                            new NxtApiTreeItem('key', 'leaf', '', '', 'nextApiTreeItem', ['thisPathKey'], TreeItemCollapsibleState.Collapsed)
+                        );
+                    }
+
+                    // add DELETE method?
+                }
 
 
             } else {
@@ -131,7 +186,7 @@ export class NextApiTreeProvider implements TreeDataProvider<NxtApiTreeItem> {
                     )
                 );
 
-                if(this.pathsTreeObj) {
+                if (this.pathsTreeObj) {
                     Object.keys(this.pathsTreeObj).forEach(key => {
                         const item = new NxtApiTreeItem(key, '', '', '', 'nextApiTreeItem', [key], TreeItemCollapsibleState.Collapsed)
                         item.path = [key];
@@ -140,62 +195,55 @@ export class NextApiTreeProvider implements TreeDataProvider<NxtApiTreeItem> {
                         );
                     });
                 }
-
-
-                // const branches: NxtApiTreeItem[] = [];
-                //     Object.entries(this.oai!.paths).forEach(([key, value]) => {
-
-                //         // split each path into paths
-                //         const paths = key.split('/');
-                //         const paths2 = paths.shift();
-                //         if (paths.length > 1) {
-                //             // this is a path so return a path tree item
-                //             branches.push(
-                //                 new NxtApiTreeItem(`/${paths[0]}`, '', '', '', 'nextApiTreeItem', TreeItemCollapsibleState.Collapsed)
-                //             );
-                //         } else {
-
-                //             const toolTip = new MarkdownString()
-                //                 .appendCodeblock(jsyaml.dump(value, { indent: 4 }), 'yaml');
-                //             const methods = Object.keys(value).join('/');
-
-                //             treeItems.push(
-                //                 new NxtApiTreeItem(key, methods, toolTip, '', 'nextApiItem', TreeItemCollapsibleState.None,
-                //                     { command: 'f5.makeRequest', title: '', arguments: [{ url: key }] }
-                //                 )
-                //             );
-                //         }
-
-
-                //     });
-                //     const newS = new Set(branches);
-                //     treeItems.push(...unique(branches, (a, b) => (a.label === b.label)));
-                // }
             }
-
-
             return treeItems;
         }
-
-
-
-
     }
 }
 
 
-function pathTree(paths: string[]) {
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function pathTree(paths: string[]) {
     // type the tree we are making.  4 levels seems sufficient for now...
-    const pathsTreeObj: {
-        [key: string]: {
-            [key: string]: {
-                [key: string]: {
-                    [key: string]: {}
-                }
-            }
-        }
-    } = {};
+    const pathsTreeObj: { [key: string]: { [key: string]: { [key: string]: { [key: string]: { [key: string]: { [key: string]: { }}}}}}} = {};
 
     // https://codereview.stackexchange.com/questions/158134/generate-a-nested-structure-based-on-a-list-of-file-paths
     // https://codereview.stackexchange.com/a/277982
@@ -204,7 +252,7 @@ function pathTree(paths: string[]) {
     paths.forEach(el => {
         const el2 = el.split('/')
         el2.shift() // shift the first element off '/'
-        
+
         // remove duplicates and nest into an object tree
         el2.reduce((o, k) => (o[k] = o[k] || {}), pathsTreeObj);
     });
@@ -214,7 +262,7 @@ function pathTree(paths: string[]) {
 
 
 /**
- * bigiq class tree item
+ * open api class tree item
  */
 class NxtApiTreeItem extends TreeItem {
     constructor(
@@ -231,95 +279,153 @@ class NxtApiTreeItem extends TreeItem {
     }
 }
 
-
-function unique(a: NxtApiTreeItem[], fn: (arg0: NxtApiTreeItem, arg1: NxtApiTreeItem) => any) {
-    if (a.length === 0 || a.length === 1) {
-        return a;
-    }
-    if (!fn) {
-        return a;
-    }
-
-    for (let i = 0; i < a.length; i++) {
-        for (let j = i + 1; j < a.length; j++) {
-            if (fn(a[i], a[j])) {
-                a.splice(i, 1);
-            }
-        }
-    }
-    return a;
-}
-
-
-
-
-
 /**
- * searches object for key
- * 
- * *** todo: update path to be array, not dot(.) notation
- *  - pretty sure this is complete...
- * 
- * @param obj to search
- * @param key to find
- * @param return [{ path: string, key: string, value: string }]
+ * open api post details for json editor magic
  */
- export function pathValueFromKey(obj: unknown, key: string, path?: string): any {
-
-    console.log('inbound pathValueFromKey', key, path)
-    const results: any[] = [];
-
-    // const objType = typeof obj;
-    // if (objType !== "object") {
-    //     logger.error(`findValueFromKey function expected object, got: ${objType}`);
-    //     return;
-    // }
-
-    /**
-     * iterate through json tree looking for key match
-     */
-    function findKey(obj: any, key: string, path?: string) {
-
-        /**
-         * if the current object we are on has the key we are looking for,
-         * push result details
-         * 
-         */
-        if (obj.hasOwnProperty(key)) {
-            return {
-                path,
-                key,
-                value: obj[key]
-            }
-        }
-
-        /**
-         * append path as we iterate
-         */
-        path = `${path ? path + "." : ""}`;
-        console.log(path)
-
-        for (const k in obj) {
-            if (obj.hasOwnProperty(k)) {
-                if (obj[k] && typeof obj[k] === "object") {
-                    findKey(obj[k], key, `${path}${k}`);
-                }
-            }
-        }
-    }
-
-    // call functoin to start iteration
-    return findKey(obj, key)
-
-    // if (results.length = 1) {
-    //     // return array of results
-    //     return results[0];
-    // } else {
-    //     logger.error(`pathValueFromKey found more than one match, returning first, full list: ${results}`)
-    //     return results[0];
-    // }
-
+export class OaiPost {
+    constructor(
+        public path: string,
+        public schemaRef: string,
+        public example?: string | unknown,
+    ) {}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function unique(a: NxtApiTreeItem[], fn: (arg0: NxtApiTreeItem, arg1: NxtApiTreeItem) => any) {
+//     if (a.length === 0 || a.length === 1) {
+//         return a;
+//     }
+//     if (!fn) {
+//         return a;
+//     }
+
+//     for (let i = 0; i < a.length; i++) {
+//         for (let j = i + 1; j < a.length; j++) {
+//             if (fn(a[i], a[j])) {
+//                 a.splice(i, 1);
+//             }
+//         }
+//     }
+//     return a;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /**
+//  * searches object for key
+//  * 
+//  * *** todo: update path to be array, not dot(.) notation
+//  *  - pretty sure this is complete...
+//  * 
+//  * @param obj to search
+//  * @param key to find
+//  * @param return [{ path: string, key: string, value: string }]
+//  */
+// export function pathValueFromKey(obj: unknown, key: string, path?: string): any {
+
+//     console.log('inbound pathValueFromKey', key, path)
+//     const results: any[] = [];
+
+//     // const objType = typeof obj;
+//     // if (objType !== "object") {
+//     //     logger.error(`findValueFromKey function expected object, got: ${objType}`);
+//     //     return;
+//     // }
+
+//     /**
+//      * iterate through json tree looking for key match
+//      */
+//     function findKey(obj: any, key: string, path?: string) {
+
+//         /**
+//          * if the current object we are on has the key we are looking for,
+//          * push result details
+//          * 
+//          */
+//         if (obj.hasOwnProperty(key)) {
+//             return {
+//                 path,
+//                 key,
+//                 value: obj[key]
+//             }
+//         }
+
+//         /**
+//          * append path as we iterate
+//          */
+//         path = `${path ? path + "." : ""}`;
+//         console.log(path)
+
+//         for (const k in obj) {
+//             if (obj.hasOwnProperty(k)) {
+//                 if (obj[k] && typeof obj[k] === "object") {
+//                     findKey(obj[k], key, `${path}${k}`);
+//                 }
+//             }
+//         }
+//     }
+
+//     // call functoin to start iteration
+//     return findKey(obj, key)
+
+//     // if (results.length = 1) {
+//     //     // return array of results
+//     //     return results[0];
+//     // } else {
+//     //     logger.error(`pathValueFromKey found more than one match, returning first, full list: ${results}`)
+//     //     return results[0];
+//     // }
+
+// }
 
 
 
@@ -332,7 +438,7 @@ function unique(a: NxtApiTreeItem[], fn: (arg0: NxtApiTreeItem, arg1: NxtApiTree
  * @param path to fetch value
  * @param obj containing path/value to get
  */
- export function deepGet(obj: unknown, path: string[]): unknown {
+export function deepGet(obj: unknown, path: string[]): unknown {
 
     const xxx = index(obj, path);
     return xxx;
@@ -341,10 +447,12 @@ function unique(a: NxtApiTreeItem[], fn: (arg0: NxtApiTreeItem, arg1: NxtApiTree
         if (typeof is === 'string') {
             return index(obj, is.split('.'), value);
         } else if (is.length === 1 && value !== undefined) {
+            // @ts-expect-error
             return obj[is[0]] = value;
         } else if (is.length === 0) {
             return obj;
         } else {
+            // @ts-expect-error
             return index(obj[is[0]], is.slice(1), value);
         }
     }
